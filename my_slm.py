3e16e05b4c3f8401c537abe30f438cc7b91332f1
Ryan Wise <ryanmwise1@gmail.com>
2017-06-25 09:34:05 -0700

    WIP

--- my_slm.py
+++ my_slm.py
@@ -26,20 +26,6 @@ NOT_FOUND_LIKELIHOOD = -100
 
 
 def slm_recognize(probabilities: dict, test_set: SinglesData):
-    """ Recognize test word sequences from word models set
-
-   :param models: dict of trained models
-       {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
-   :param test_set: SinglesData object
-   :return: (list, list)  as probabilities, guesses
-       both lists are ordered by the test set word_id
-       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
-           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
-            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
-            ]
-       guesses is a list of the best guess words ordered by the test set word_id
-           ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
-   """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
 
     @lru_cache
